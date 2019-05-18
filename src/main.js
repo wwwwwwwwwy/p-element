75d413ea190512c27a75dce83a73e7640d87bec5
wangying <leosmilence@163.com>
2019-05-18 15:51:49 +0800

    开始定制样式

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'element-ui/lib/theme-chalk/index.css';
+// import 'element-ui/lib/theme-chalk/index.css';
+import './theme/index.css';
 import ElementUI from 'element-ui';
 Vue.use(ElementUI);
 
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Button from './views/Button.vue'
 
 Vue.use(Router)
 
@@ -20,6 +21,14 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/button',
+      name: 'button',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Button.vue')
     }
   ]
 })
